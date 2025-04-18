28682461d27cbd72149154fb9288225db604700a
Eveline Murta <131032885+EveMurta@users.noreply.github.com>
2025-04-18 15:14:03 -0300

    Commit projeto completo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  function verificaLogin(){
+
+    if (email === 'eveline.amorim@pucpr.br' && senha === '123456') {
+      setMensagem('Acessado com sucesso!');
+    } else {
+      setMensagem('Usuário ou senha incorretos!');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login</h1>
+
+      <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <br />
+
+      <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)}/>
+      <br />
+
+      <button onClick={verificaLogin}> Acessar </button>
+
+      <p>{mensagem}</p>
     </div>
   );
 }
